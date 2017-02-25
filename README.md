1bedf6ae84fb48244193cecc005c1f3bcc75ed6a
Ton Theonelove <tonlove5233@gmail.com>
2017-02-26 00:07:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AndroidMaterialDesignSlidingTabs
+Android Material Design Sliding Tab
+
+Complete Tutorial: http://www.viralandroid.com/2016/04/android-material-design-sliding-tabs-example.html
---
+++ app/src/main/java/ton/com/materialdesignslidingtabs/MainActivity.java
@@ -0,0 +1,96 @@
+package ton.com.materialdesignslidingtabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+
+public class MainActivity extends ActionBarActivity implements MaterialTabListener {
+
+    MaterialTabHost tabHost;
+    ViewPager viewPager;
+    ViewPagerAdapter androidAdapter;
+    Toolbar toolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //android toolbar
+        toolBar = (android.support.v7.widget.Toolbar) this.findViewById(R.id.toolBar);
+        this.setSupportActionBar(toolBar);
+
+        //tab host
+        tabHost = (MaterialTabHost) this.findViewById(R.id.tabHost);
+        viewPager = (ViewPager) this.findViewById(R.id.viewPager);
+
+        //adapter view
+        androidAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(androidAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int tabposition) {
+                tabHost.setSelectedNavigationItem(tabposition);
+            }
+        });
+
+        //for tab position
+        for (int i = 1; i < androidAdapter.getCount(); i++) {
+            tabHost.addTab(
+                    tabHost.newTab()
+                            .setText(androidAdapter.getPageTitle(i))
+                            .setTabListener(this)
+            );
+        }
+    }
+
+    //tab on selected
+    @Override
+    public void onTabSelected(MaterialTab materialTab) {
+
+        viewPager.setCurrentItem(materialTab.getPosition());
+    }
+
+    //tab on reselected
+    @Override
+    public void onTabReselected(MaterialTab materialTab) {
+
+    }
+
+    //tab on unselected
+    @Override
+    public void onTabUnselected(MaterialTab materialTab) {
+
+    }
+
+    // view pager adapter
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        public Fragment getItem(int num) {
+            return new AndroidFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 8;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int tabposition) {
+            return "Tab " + tabposition;
+        }
+
+    }
+}
